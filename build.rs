a66261199ea11d38340b88bd5934f4ecfbd6ba61
evpobr <evpobr@gmail.com>
2018-11-05 13:34:40 +0500

    Seach sndfile library with Vcpkg on Windows platform

--- build.rs
+++ build.rs
@@ -1,1 +1,16 @@
-fn main() {}
+extern crate vcpkg;
+
+fn main() {
+    if cfg!(target_env = "msvc") {
+        if try_vcpkg() {
+            return;
+        }
+    }
+}
+
+fn try_vcpkg() -> bool {
+    match vcpkg::find_package("libsndfile") {
+        Ok(_) => true,
+        _ => false
+    }
+}
